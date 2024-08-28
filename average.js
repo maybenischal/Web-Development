8bca165be7480145b51d8fc068febaa9a829ced7
Nischal <nischaldhakal1409@gmail.com>
2024-08-28 08:39:59 +0545

    Initial Commit

---
+++ average.js
@@ -0,0 +1,41 @@
+function getAverage(scores) {
+    let sum = 0;
+  
+    for (const score of scores) {
+      sum += score;
+    }
+  
+    return sum / scores.length;
+  }
+  
+  function getGrade(score) {
+    if (score === 100) {
+      return "A++";
+    } else if (score >= 90) {
+      return "A";
+    } else if (score >= 80) {
+      return "B";
+    } else if (score >= 70) {
+      return "C";
+    } else if (score >= 60) {
+      return "D";
+    } else {
+      return "F";
+    }
+  }
+  
+  function hasPassingGrade(score) {
+    return getGrade(score) !== "F";
+  }
+  
+  function studentMsg(totalScores, studentScore) {
+   let average = getAverage(totalScores);
+    let grade = getGrade(studentScore);
+    let passStatus = hasPassingGrade(studentScore) ? "passed" : "failed";
+    
+    return `Class average: ${average.toFixed(1)}. Your grade: ${grade}. You ${passStatus} the course.`;
+  }
+  console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
+  console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 87));
+
+  
